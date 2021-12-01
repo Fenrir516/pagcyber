ed60bed66b2a75d54de11aa579324d1b524e52b6
Fernando Estrada <91513700+Fenrir516@users.noreply.github.com>
2021-12-01 12:40:34 -0700

    Add files via upload

---
+++ Computadora.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>0601 Estrada Aldama Fernando</title>
+    <link rel="stylesheet" href="fuentes.css">
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="#"><img src="img/ass.png" alt="FalconMasters"></a>
+			</div>
+			<nav>
+				<ul>
+                    <li><a href="index.php">Inicio</a></li>
+					<li><a href="Computadora.php">Computadora</a></li>
+					<li><a href="Renta.php">Renta</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<section class="main">
+		<div class="wrapp">
+			<div class="mensaje">
+				<h1>Computadora</h1>
+			</div>
+
+			<div class="articulo">
+				<article>
+					<p>Llena el seiguiente formulario en el que nos diras las especificaciones de la computadora que usaras en nuestras instalaciones  </p>
+					<br/>
+				    <form class ="form" >
+						<label>Procesador</label> <input type="text" name="name">
+						<br>
+						<label>Tarjeta grafica</label> <input type="text" name="text">
+					<br>
+					<label>RAM</label> <input type="text" name="k">
+					<br> 
+					<label>Disco duro</label> <input type ="text">
+					<br>
+					 <label>Monitor</label> <input type ="text">
+					<br>
+					<input type="submit" >
+				</form>
+					<br/>
+					
+					<br/>
+					<p>
+                        
+                    </p>
+				</article>
+			</div>
+
+			<aside>
+				<div class="widget">
+					<h3>Horarios</h3>
+					<ul>
+						<li><a href="#">Lunes 8am/9pm</a></li>
+						<li><a href="#">Martes 8am/9pm</a></li>
+						<li><a href="#">Miercoles 8am/9pm</a></li>
+						<li><a href="#">Jueves 8am/9pm</a></li>
+						<li><a href="#">Viernes 8am/9pm</a></li>
+						<li><a href="#">Sabado 8am/12am</a></li>
+						<li><a href="#">Domingo 5pm/8pm</a></li>
+					</ul>
+				</div>
+			</aside>
+		</div>
+	</section>
+	<footer>
+		<div class="wrapp">
+            <p> No lista 01 </p>
+            <p> // SUBMÓDULO 2
+                Desarrolla aplicaciones Web con conexión a bases de datos</p>
+            <p>https://github.com/Fenrir516 </p>
+		</div>
+	</footer>
+</body>
+</html>
---
+++ Renta.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>0601 Estrada Aldama Fernando</title>
+    <link rel="stylesheet" href="fuentes.css">
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="#"><img src="img/ass.png" alt="FalconMasters"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="Computadora.php">Computadora</a></li>
+					<li><a href="Renta.php">Renta</a></li>
+					
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<section class="main">
+		<div class="wrapp">
+			<div class="mensaje">
+				<h1>Renta</h1>
+			</div>
+
+			<div class="articulo">
+				<article>
+					<p>Contesta el siguiente formulario para poder rebtar una computadora ya sea de alta o media gama y por cuanto tiempo para poder darte el costo por la renta</p>
+					<br/>
+				    <form class ="form" >
+					<label>Tipo de computadora (Alta/Media)</label> <input type="text" name="name">
+						<br>
+						<label>Cuantas horas</label> <input type="number" >
+					<br>
+					<label>Num de Computadora</label> <input type="number" name="k">
+					<br> 
+					<label>Total a pagar</label> <input type ="number">
+					
+					<br>
+					<input type="submit" value="Pagar" >
+				</form>
+					<br/>
+					
+				</article>
+			</div>
+
+			<aside>
+				<div class="widget">
+					<h3>Horarios</h3>
+					<ul>
+						<li><a href="#">Lunes 8am/9pm</a></li>
+						<li><a href="#">Martes 8am/9pm</a></li>
+						<li><a href="#">Miercoles 8am/9pm</a></li>
+						<li><a href="#">Jueves 8am/9pm</a></li>
+						<li><a href="#">Viernes 8am/9pm</a></li>
+						<li><a href="#">Sabado 8am/12am</a></li>
+						<li><a href="#">Domingo 5pm/8pm</a></li>
+					</ul>
+				</div>
+			</aside>
+		</div>
+	</section>
+	<footer>
+		<div class="wrapp">
+            <p> No lista 01 </p>
+            <p> // SUBMÓDULO 2
+                Desarrolla aplicaciones Web con conexión a bases de datos</p>
+            <p>https://github.com/Fenrir516</p>
+		</div>
+	</footer>
+</body>
+</html>
